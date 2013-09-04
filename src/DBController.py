cb0099df129169310d63b38de71ab589c858c7ac
rpetersburg <rpetersburg@gmail.com>
2013-09-04 13:35:49 -0400

    Added faster couchDB iterator. Changed particleData type

--- src/DBController.py
+++ src/DBController.py
@@ -10,38 +10,60 @@ class DBController():
         self.couch = couchdb.Server( self.couchURL )
         self.db = self.couch['history_mjdscm']
         self.dataArray = []
-        self.timeArray = []
 
     
-    def appendParticleData( self, particleRec, particleSize, dataArray, timeArray ):
+    def getParticleData( self, particleRec, index, particleSize ):
         try:
-            for item in self.db[ particleRec ]['adcs']:
-                if ('DR ' + particleSize + ' um count') in item:
-                    dataArray.append( item['DR ' + particleSize + ' um count'] )
-                    timeArray.append( self.db[ particleRec ][ 'time' ] )
+            return {'count': self.db[particleRec]['adcs'][index]['DR ' + particleSize + ' um count'] , 'time': self.db[particleRec]['time'] }
         except KeyError, TypeError:
             print 'Invalid Particle Size'
+            return {'count': None, 'time': None}
+
 
     def iterator( self, particleSize ):
         n = 0
-        print 'Starting'
-        for rec in self.db:
-            print 'Iterating'
-            if n > 10:
+        for rec in self.couchdb_pager(self.db):
+            if n>10:
                 break
-            elif 'adcs' in rec:
-                print "Hooray"
-                appendParticleData( rec, particleSize, self.dataArray, self.timeArray )
+            if 'adcs' in self.db[ rec ]:
+                for item in self.db[ rec ]['adcs']:
+                    if ('DR ' + particleSize + ' um count') in item:
+                        self.dataArray.append( self.getParticleData( rec, self.db[rec]['adcs'].index(item), particleSize ) )
+                        n+=1
+
+            
+    def couchdb_pager(self, db, view_name='_all_docs', startkey=None, startkey_docid=None, endkey=None, endkey_docid=None, bulk=5000):
+        # Request one extra row to resume the listing there later.
+        options = {'limit': bulk + 1}
+        if startkey:
+            options['startkey'] = startkey
+            if startkey_docid:
+                options['startkey_docid'] = startkey_docid
+        if endkey:
+            options['endkey'] = endkey
+            if endkey_docid:
+                options['endkey_docid'] = endkey_docid
+        done = False
+        while not done:
+            view = self.db.view(view_name, **options)
+            rows = []
+            # If we got a short result (< limit + 1), we know we are done.
+            if len(view) <= bulk:
+                done = True
+                rows = view.rows
             else:
-                print 'Oh no!'
-            n += 1
-                
+                # Otherwise, continue at the new start position.
+                rows = view.rows[:-1]
+                last = view.rows[-1]
+                options['startkey'] = last.key
+                options['startkey_docid'] = last.id
+
+            for row in rows:
+                yield row.id
+                    
 
 
 
 dbController = DBController()
 dbController.iterator('0.5')
 print dbController.dataArray
-##dates = dates.date2num(timeArray)
-print dbController.timeArray
-##print dates
