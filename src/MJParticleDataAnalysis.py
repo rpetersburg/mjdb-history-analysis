3a13e4f94fb1d2553ecc307666b6f0df4545bd57
rpetersburg <rpetersburg@gmail.com>
2013-09-16 11:30:40 -0400

    Dates can now be included in the arguments

--- src/MJParticleDataAnalysis.py
+++ src/MJParticleDataAnalysis.py
@@ -8,17 +8,21 @@ class MJParticleDataAnalysis:
         self.dbController = DBController.DBController()
         self.dataAnalysis = DataAnalysis.DataAnalysis()
 
-    def run(self, particleSize='0.5'):
+    def run(self, particleSize='2.0', startDate='09-18-2012', endDate=datetime.utcnow().strftime('%m-%d-%Y') ):
         dataArray = self.dbController.getParticleData( particleSize )
 
-        startDate = datetime(2013,6,1)
-        endDate = datetime(2013,9,8)
+        startDate = datetime.strptime(startDate,'%m-%d-%Y')
+        endDate = datetime.strptime(endDate,'%m-%d-%Y')
         allData = self.dataAnalysis.getAnalysisByDate(dataArray,startDate,endDate)
 
         self.dataAnalysis.plotAllData( allData, particleSize )
         
         dataDictionary = self.dataAnalysis.organizeDataArray( dataArray, startDate, endDate )
         self.dataAnalysis.graphDataDictionary( dataDictionary, particleSize )
+
+    def runAll(self):
+        for particleSize in ['0.3','0.5','0.7','1.0','2.0','5.0']:
+            self.run(particleSize)
 
     def saveParticleData( self ):
         for particleSize in ['0.3','0.5','0.7','1.0','2.0','5.0']:
@@ -28,10 +32,14 @@ class MJParticleDataAnalysis:
 if __name__ == '__main__':
     app = MJParticleDataAnalysis()
     if len( sys.argv ) == 1:
-        app.run()
+        app.runAll()
     elif len( sys.argv ) == 2 and sys.argv[1] == 'save':
         app.saveParticleData()
     elif len( sys.argv ) == 2 and sys.argv[1] != 'save':
         app.run( str(sys.argv[1]) )
+    elif len( sys.argv ) == 3:
+        app.run( str(sys.argv[1]), str(sys.argv[2]) )
+    elif len( sys.argv ) == 4:
+        app.run( str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]) )
     else:
         print 'Invalid Arguments'
